2293808aa33cc3511509fb00e59877a4c0485863
nathanx98 <31714561+nathanx98@users.noreply.github.com>
2021-09-08 00:11:05 -0400

    Update DemoServlet.java

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -20,12 +20,44 @@ import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.cloud.spanner.DatabaseClient;
+import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.ResultSet;
+import com.google.cloud.spanner.Spanner;
+import com.google.cloud.spanner.SpannerOptions;
+import com.google.cloud.spanner.Statement;
+
 
 public class DemoServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
+    
+    String instanceId = "project";
+    String databaseId = "os-product-db";
+    
+    
+    // Instantiates a client
+    SpannerOptions options = SpannerOptions.newBuilder().build();
+    Spanner spanner = options.getService();
+    String results = "\n";
+    try {
+      // Creates a database client
+      DatabaseClient dbClient =
+          spanner.getDatabaseClient(DatabaseId.of(options.getProjectId(), instanceId, databaseId));
+      // Queries the database
+      ResultSet resultSet = dbClient.singleUse().executeQuery(Statement.of("SELECT *"));
+
+      // Prints the results
+      while (resultSet.next()) {
+        results += resultSet.getLong(0) + "\n";
+      }
+    } finally {
+      // Closes the client which will free up the resources used
+      spanner.close();
+    }
+  
     resp.setContentType("text/plain");
-    resp.getWriter().println("{ \"name\": \"World\" }");
+    resp.getWriter().println("{ \"name\": \"" + results + "\" }");
   }
 }
